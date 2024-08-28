0ee8e41bbdb948212bb4d464962f9ae77135f729
veliside <biposto_996@yahoo.gr>
2024-08-29 00:01:34 +0300

    Introduce types

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+declare module '*.scss' {
+  const classes: {[key: string]: string};
+
+  export default classes;
+}
+
+declare module '*.svg' {
+  const value: string;
+
+  export default value;
+}
